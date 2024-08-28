3537e6b89991b78693081e7f34494eb162eddcb4
Khadeeja beevi <khadeejabeevinavas@gmail.com>
2024-08-28 09:44:38 +0530

    done

--- Learn/string_functions.py
+++ Learn/string_functions.py
@@ -20,7 +20,12 @@ print(x) # true
 # print the name along with a text using format function
 name="khadeeja"
 txt="my name is {} and my age is {}"
+txt1="my name is {0} and my age is {1}"
+txt2="my name is {1} and my age is {0}"
 age=30
 print(txt.format(name,age)) # my name is khadeeja and my age is 30
+print(txt1.format(name,age)) # my name is khadeeja and my age is 30
+print(txt2.format(name,age)) # my name is 30 and my age is khadeeja
+
 
 
