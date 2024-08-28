a75caabf8e743dff429d6497aac8f57ce6205107
Khadeeja beevi <khadeejabeevinavas@gmail.com>
2024-08-28 09:57:36 +0530

    yes

---
+++ Learn/boolean.py
@@ -0,0 +1,13 @@
+x=True
+y=not True
+print(x) #True
+print(y) #False
+
+#only 0 is false all other nuumbers are true
+x=bool(1)   
+y=bool(-5)
+print(x) #True
+print(y) #True
+
+
+
--- Learn/string_functions.py
+++ Learn/string_functions.py
@@ -27,5 +27,32 @@ print(txt.format(name,age)) # my name is khadeeja and my age is 30
 print(txt1.format(name,age)) # my name is khadeeja and my age is 30
 print(txt2.format(name,age)) # my name is 30 and my age is khadeeja
 
+#print the name along with quotes we use an escape slash
+name1="\"khadeeja\""
+print(name1) # "khadeeja"
+
+# find the location of character
+print(name.index("a")) # 2
+
+# checking all are numbers or not
+num="123"
+print(num.isdigit()) #True
+#checking all are alphabets or not
+print(name.isalpha()) #True
+print(num.isalpha()) #False
+#checking all are lowercase or not 
+print(name.islower()) #True
+#checking all are uppercase or not
+print(name.isupper()) # False
+
+#chcking beginning or ending with
+print(name.startswith("kh")) #True
+print(name.endswith("ja"))#True
+
+
+
+
+
+
 
 
